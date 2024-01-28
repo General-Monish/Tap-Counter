ad445d0631d53f16e52b65d2d78a9e487e2f418b
General-Monish <monissam28@gmail.com>
2024-01-29 03:22:21 +0530

    Added Music And Animations of UI

--- Scripts/MainMenuUI.cs
+++ Scripts/MainMenuUI.cs
@@ -9,6 +9,9 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] private GameObject howToPlay;
     [SerializeField] private GameObject credits;
     [SerializeField] private GameObject mainMenu;
+    [SerializeField] private AudioClip audioClip;
+    [SerializeField] private AudioSource aD;
+    [SerializeField] private Animator anim;
     
     // Start is called before the first frame update
     void Start()
@@ -27,30 +30,43 @@ public class MainMenuUI : MonoBehaviour
 
     public void SettingBtnClicked()
     {
+        SoundUI();
         mainMenu.SetActive(false);
         settings.SetActive(true);
+        anim.SetTrigger("in");
     }
     public void HotToPlayBtnClicked()
     {
+        SoundUI();
         mainMenu.SetActive(false);
         howToPlay.SetActive(true);   
     }
     public void CreditsBtnClicked()
     {
+        SoundUI();
         mainMenu.SetActive(false);
         credits.SetActive(true);
     } 
 
     public void BackBtnClicked()
     {
+        SoundUI();
+        anim.SetTrigger("out");
         mainMenu.SetActive(true);
-        settings.SetActive(false);
+       // settings.SetActive(false);
+       
         howToPlay.SetActive(false);
         credits.SetActive(false);
     }
 
     public void PlayBtnClicked()
     {
+        SoundUI();
         SceneManager.LoadScene("Game");
+    }
+
+    private void SoundUI()
+    {
+        aD.PlayOneShot(audioClip);
     }
 }
